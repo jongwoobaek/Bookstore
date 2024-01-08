90943ddc91f0bf21daee0d43680958a8467fa358
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-01-09 01:23:24 +0900

    feat: Add BookstoreException to bookstoreException package

---
+++ bookstoreException/BookstoreException.java
@@ -0,0 +1,13 @@
+package bookstore.bookstoreException;
+
+public class BookstoreException extends RuntimeException {
+    private ErrorCode errorCode;
+
+    public BookstoreException(ErrorCode code) {
+        this.errorCode = code;
+
+        System.out.println("\n*************** error 발생 *************** ");
+        System.out.println(errorCode.getCode()+" / "+errorCode.getMessage()+" / "+errorCode.getStatus());
+        System.out.println("*****************************************");
+    }
+}
