133b14bcf5d43a345440a7954379bd27ca0401a5
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-01-10 14:11:05 +0900

    feat: Add isValidName method to BookstoreExceptionList

--- bookstoreException/BookstoreExceptionList.java
+++ bookstoreException/BookstoreExceptionList.java
@@ -6,6 +6,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BookstoreExceptionList {
+    public static boolean isValidName(String name) {
+        String regex = "^[a-zA-Z가-힣]*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+
+        if (matcher.matches()) return false;
+
+        return true;
+    }
+
     public static boolean isValidPhoneNumber(String phoneNumber) {
         String regex = "^\\d{3}-\\d{3,4}-\\d{4}$";
         Pattern pattern = Pattern.compile(regex);
